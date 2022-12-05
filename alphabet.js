59ee8e0645d7605537243f16fb6ff5d9da9211ce
Breakingb0x3s <115906251+Breakingb0x3s@users.noreply.github.com>
2022-12-04 18:08:25 -0700

    Add files via upload

--- alphabet.js
+++ alphabet.js
@@ -1,13 +1,19 @@
-let abcs = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, X, Y, Z, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, x, y, z];
-const colors = ['red', 'blue', 'green', 'purple', 'pink', 'beige', 'yellow', 'orange']
+let abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'];
+const colors = ['red', 'blue', 'green', 'purple', 'pink', 'beige', 'yellow', 'orange'];
 let letter = document.getElementById('alphabet');
 let goodJob = document.getElementById('good');
 let tryAgain = document.getElementById('try');
 
+
 let getRandomABC = () =>{
-    let a = Math.floor(Math.random * abcs.length);
-    letter.innerHTML = abcs[a];
-    abcs.slice(a, 1);
+    let b = document.createElement('li');
+    b.innerHTML = letter.innerHTML;
+    document.getElementById('goodWords').appendChild(b);
+    alert('You got it, good job Ivory')
+    let a = Math.floor(Math.random() * abc.length);
+    letter.innerHTML = abc[a];
+    abc.slice(a, 1);
+    return abc
 }
 
 let change = () =>{
